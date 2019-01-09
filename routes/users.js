4cdb732bfa8f7dd1c167294ad8ee9f3a80c6f2f8
angelpray <1136472002@qq.com>
2019-01-09 19:19:41 +0800

    view

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/:name', (req, res) => {
-  res.send(`hello ${req.params.name}`);
+  res.render('users', {
+    name: req.params.name
+  });
 });
 
 module.exports = route;
